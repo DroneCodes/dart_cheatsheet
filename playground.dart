95acf75f7d8d723889d3098fef73c6a242742231
Ogunyemi Oluwafisayomi <oluwafisayomiogunyemi@gmail.com>
2022-01-18 00:17:52 +0100

    Add files via upload

---
+++ playground.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Types in Dart
+  var firstame = "Fisayo";
+  String lastName = "Ogunyemi";
+  int age = 17;
+
+  // concatenation in Dart
+  print(firstame + ' ' + lastName);
+}
+
+
